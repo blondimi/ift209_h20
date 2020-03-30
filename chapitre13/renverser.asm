11bef4ea6c2eb952fb15f129de6d7761fef59575
Michael Blondin <michael.blondin@tum.de>
2020-03-30 11:02:29 -0400

    Réorganisation pour codes de condition

--- chapitre13/renverser.asm
+++ chapitre13/renverser.asm
@@ -2,16 +2,17 @@ dir:      .rs 1 ; vaut 0 (gauche) ou 1 (droite)
 tuile:    .rs 4 ; (posY, identifiant, attributs, posX)
 
 renverser:                       ; renverser()
-    lda   dir                    ; {
+    ldx   #2                     ; {
+    lda   dir                    ;
     cmp   #0                     ;
-    ldx   #2                     ;
-    lda   tuile, x               ;
     bne   renverser_un           ;   if (dir == 0)
 renverser_zero:                  ;   {
+    lda   tuile, x               ;
     and   #%10111111             ;     a = tuile[2] & 0xBF
     jmp   renverser_fin          ;   }
 renverser_un:                    ;   else
+    lda   tuile, x               ;   {
     ora   #%01000000             ;     a = tuile[2] | 0x40
-renverser_fin:                   ;
+renverser_fin:                   ;   }
     sta   tuile, x               ;   tuile[2] = a
     rts                          ; }
